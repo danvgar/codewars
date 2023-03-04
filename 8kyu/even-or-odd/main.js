faf9bda47b634647007d984ba816a74d47aae96c
Daniel Garcia <danvgar@gmail.com>
2023-03-03 22:27:26 -0800

    folder restructure

--- 8kyu/even-or-odd/main.js
+++
@@ -1,9 +0,0 @@
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-
-function evenOrOdd(number) {
-    return number % 2 == 0 ? "Even" : "Odd"
-}
-
-evenOrOdd(42)
-evenOrOdd(1)
-evenOrOdd(0)
