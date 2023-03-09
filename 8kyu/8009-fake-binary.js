daaa6d8df718231410d0be3d858949be2f17f1c2
Daniel Garcia <danvgar@gmail.com>
2023-03-08 23:34:57 -0800

    daily codewars challenge

---
+++ 8kyu/8009-fake-binary.js
@@ -0,0 +1,51 @@
+// https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+// First Attempt - Mar 8, 2023
+
+// =============
+// Problem
+// =============
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+// function fakeBin(x){
+
+// }
+
+// ==============
+// Solution 1
+// ==============
+
+function fakeBin(x){
+    let array = x.split("")
+    
+    for(let i = 0; i < array.length; i++) {
+        
+        if (parseInt(array[i]) < 5) {
+            array[i] = '0'
+        } else {
+            array[i] = '1'
+        } 
+    }
+    
+    let newString = array.join('')
+    return newString
+} 
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
+        assert.strictEqual(fakeBin('509321967506747'), '101000111101101'); 
+        assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
+    })
+});
