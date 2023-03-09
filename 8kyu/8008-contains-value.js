621c4884b2b0caa81bd8afa5a6facd31a364352a
Daniel Garcia <danvgar@gmail.com>
2023-03-08 23:34:49 -0800

    daily codewars - need to retry

---
+++ 8kyu/8008-contains-value.js
@@ -0,0 +1,58 @@
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+// First Attempt - Mar 8, 2023
+
+// =============
+// Problem
+// =============
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// function check(a, x) {
+//     // your code here
+//   }
+
+// ==============
+// Solution 1
+// ==============
+
+function check(a, x) {
+    
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === x) {
+            console.log(true) 
+            break;
+        } else {
+            console.log(false) 
+            continue;
+        }
+    }
+}
+
+check([66, 101], 66)
+check([101, 45, 75, 105, 99, 107], 107)
+check(['t', 'e', 's', 't'], 'e')
+check(['what', 'a', 'great', 'kata'], 'kat')
+
+// need to try again
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(check([66, 101], 66), true);
+        assert.strictEqual(check([101, 45, 75, 105, 99, 107], 107), true);
+        assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+        assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+    })
+})
