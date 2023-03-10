6be868097a9c088e3f33cccacff1777e61409820
Daniel Garcia <danvgar@gmail.com>
2023-03-09 22:33:21 -0800

    daily codewars challenge

---
+++ 8kyu/8010-double-array.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+// First Attempt - Mar 9, 2023
+
+// =============
+// Problem
+// =============
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// function maps(x){
+
+// }
+
+// ==============
+// Solution 1
+// ==============
+
+function maps(x){
+    let doubledArray = []
+    x.forEach((a,i) => doubledArray.push(a*2))
+    return doubledArray
+}
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", ()=>{
+    it("Fixed tests", () => {
+        assert.deepEqual(maps([1, 2, 3]), [2, 4, 6]);
+        assert.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]); 
+        assert.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]); 
+    });
+});
