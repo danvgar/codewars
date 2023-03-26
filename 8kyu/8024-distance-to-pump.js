6113c2336bb57e3721a307599c0a7acc16b83388
Daniel Garcia <danvgar@gmail.com>
2023-03-25 23:16:12 -0700

    daily codewars

---
+++ 8kyu/8024-distance-to-pump.js
@@ -0,0 +1,38 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+// First Attempt - Mar 25, 2023
+
+// =============
+// Problem
+// =============
+
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+// };
+
+
+// ==============
+// Solution 1
+// ==============
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return (distanceToPump === mpg * fuelLeft) ? true : false
+};
+
+
+// ==============
+// Tests
+// ==============
+
+const assert = require("chai").assert;
+
+describe("zeroFill", function() {
+    it("Sample Tests", function() {
+        assert.equal(zeroFuel(50, 25, 2), true);
+        assert.equal(zeroFuel(100, 50, 1), false);
+    });
+});
