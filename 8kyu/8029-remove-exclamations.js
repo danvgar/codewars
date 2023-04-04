11d6c674142a8682381f3dd548188f2aed9d0dfc
Daniel Garcia <danvgar@gmail.com>
2023-04-04 15:28:35 -0700

    daily codewars (first try)

---
+++ 8kyu/8029-remove-exclamations.js
@@ -0,0 +1,38 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+// First Attempt - Apr 4, 2023
+
+
+// =============
+// Problem
+// =============
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// function removeExclamationMarks(s) {
+// }
+
+
+// ==============
+// Solution 1
+// ==============
+
+function removeExclamationMarks(s) {
+    return s.split("").filter(a => a !== "!").join("")
+}
+
+// #firsttry
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+    it("test", () => {
+        assert.strictEqual(removeExclamationMarks("Hello World!"), "Hello World");
+    });
+});
