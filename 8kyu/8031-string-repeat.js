fa3f68d29f08b4a4be8d50b0ea7b77b87eca00af
Daniel Garcia <danvgar@gmail.com>
2023-04-11 23:06:08 -0700

    daily codewars (first try)

---
+++ 8kyu/8031-string-repeat.js
@@ -0,0 +1,48 @@
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+// First Attempt - Apr 11, 2023
+
+
+// =============
+// Problem
+// =============
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// function repeatStr (n, s) {
+//     return '';
+// }
+
+
+// ==============
+// Solution 1
+// ==============
+
+function repeatStr (n, s) {
+    let finalString = ""
+    for(let i = 0; i < n; i++) {
+        finalString += s
+    }
+    return finalString
+}
+
+// #firsttry
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", function() {
+    it ("Basic tests", function() {
+        assert.strictEqual(repeatStr(3, "*"), "***");
+        assert.strictEqual(repeatStr(5, "#"), "#####");
+        assert.strictEqual(repeatStr(2, "ha "), "ha ha ");
+    });
+});
