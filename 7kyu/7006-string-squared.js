94a7a153a44a0131a4abc925af9b99027b960463
Daniel Garcia <danvgar@gmail.com>
2023-05-06 19:28:22 -0700

    square every digit of an integer and return the result as a concatenated string

--- 7kyu/7006-string-squared.js
+++ 7kyu/7006-string-squared.js
@@ -7,9 +7,9 @@
 
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
-// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+// For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
 
-// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+// Example #2: An input of 765 will/should return 493625 because 7^2 is 49, 6^2 is 36, and 5^2 is 25. (49-36-25)
 
 // Note: The function accepts an integer and returns an integer.
 
@@ -19,22 +19,18 @@
 //     return 0;
 // }
 
+
 // ==============
 // Solution 1
 // ==============
 
 function squareDigits(num){
-    // console.log(`Original Number: ${num}`)
     num = String(num)
-    // console.log(`String Number: ${num}`)
     num = num.split("")
-    // console.log(`Split Number: ${num}`)
     for(let i = 0; i < num.length; i++) {
         num[i] = num[i] * num[i]
     }
-    // console.log(`Squared Array: ${num}`)
     num = num.join("")
-    // console.log(`Joined Array: ${num}`)
     return Number(num)
 }
 
