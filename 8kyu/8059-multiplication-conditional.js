d7c1d67e13c9d36145b1e80a1218ea7296329ee3
Daniel Garcia <danvgar@gmail.com>
2023-05-21 14:55:58 -0700

    odd even conditionals

---
+++ 8kyu/8059-multiplication-conditional.js
@@ -0,0 +1,41 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+// First Attempt - May 18, 2023
+
+
+// =============
+// Problem
+// =============
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// function simpleMultiplication(number) {
+//   // your code........
+// }
+
+
+// ==============
+// Solution 1
+// ==============
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9
+}
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic Tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(simpleMultiplication(2),16,'Should return given argument times eight...')
+    assert.strictEqual(simpleMultiplication(1),9, 'Should return given argument times nine...')
+    assert.strictEqual(simpleMultiplication(8),64,'Should return given argument times eight...')
+    assert.strictEqual(simpleMultiplication(4),32,'Should return given argument times eight...')
+    assert.strictEqual(simpleMultiplication(5),45,'Should return given argument times nine...')
+  });
+});
