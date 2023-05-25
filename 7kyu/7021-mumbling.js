41c49e508f3eb497702775b294091610a06daeea
Daniel Garcia <danvgar@gmail.com>
2023-05-24 23:38:49 -0700

    string manipulation

---
+++ 7kyu/7021-mumbling.js
@@ -0,0 +1,54 @@
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+// First Attempt - May 24, 2023
+
+
+// =============
+// Problem
+// =============
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+// function accum(s) {
+// 	// your code
+// }
+
+
+// ==============
+// Solution 1
+// ==============
+
+function accum(s) {
+  let strArr = ""
+  let i = 0
+  while (i < s.length) {
+    strArr[i] += s.charAt(i).toUpperCase() + s.charAt(i).toLowerCase().repeat(i)
+    i++
+    if(i !== s.length) {
+      strArr += "-" 
+    }
+  }
+} 
+
+// revisit
+
+
+// ==============
+// Tests
+// ==============
+
+const Test = require('@codewars/test-compat');
+
+describe("accum",function() {
+  it("Basic tests",function() {    
+    Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+  })})
