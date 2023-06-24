1027bb3bf9f96b3e30e423880db5d049951d066e
Daniel Garcia <danvgar@gmail.com>
2023-06-23 19:57:12 -0700

    manipulate characters of a string to match an example sequence

--- 7kyu/7021-mumbling.js
+++ 7kyu/7021-mumbling.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
 // First Attempt - May 24, 2023
+// Second Attempt - June 23, 2023
 
 
 // =============
@@ -24,18 +25,8 @@
 // ==============
 
 function accum(s) {
-  let strArr = ""
-  let i = 0
-  while (i < s.length) {
-    strArr[i] += s.charAt(i).toUpperCase() + s.charAt(i).toLowerCase().repeat(i)
-    i++
-    if(i !== s.length) {
-      strArr += "-" 
-    }
-  }
-} 
-
-// revisit
+  return s.split("").map((char, i) => char.toUpperCase() + char.repeat(i).toLowerCase()).join("-")
+}
 
 
 // ==============
@@ -44,11 +35,12 @@ function accum(s) {
 
 const Test = require('@codewars/test-compat');
 
-describe("accum",function() {
-  it("Basic tests",function() {    
+describe("accum", function () {
+  it("Basic tests", function () {
     Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
     Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
     Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
     Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
     Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
-  })})
+  })
+})
