18482706d7ac1e5bcb03f67b6cfbb171723f043f
Daniel Garcia <danvgar@gmail.com>
2023-09-16 16:09:34 -0700

    return the element in the array that occurs an odd number of times

---
+++ 6kyu/6013-find-odd-occurences.js
@@ -0,0 +1,83 @@
+// https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
+// First Attempt - Sept 16, 2023
+
+
+// =============
+// Problem
+// =============
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+// function findOdd(A) {
+//   //happy coding!
+//   return 0;
+// }
+
+// P: input will be an array of integers, where onle one of the integers appears an odd number of times
+// R: output should be the integer that appears an odd number of times. 
+// E: 
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// P: 
+// loop through array.
+// count frequency of each element (.filter( element => element === A[i])).length
+// if occurence % 2 === 1, return A[i]
+
+
+// ==============
+// Solution 1
+// ==============
+
+function findOdd(A) {
+  // loop through array.
+  for (let i = 0; i < A.length; i++) {
+    // count frequency of each element (.filter( element => element === A[i])).length
+    let occurence = A.filter(element => element === A[i]).length
+    console.log(occurence)
+    console.log(occurence % 2 === 1)
+    // if occurence % 2 === 1, return A[i]
+    if (occurence % 2 === 1) {
+      return A[i]
+    }
+  }
+}
+
+
+// ==============
+// Tests
+// ==============
+
+const assert = require('chai').assert;
+
+describe('Example tests', function () {
+
+  function doTest(a, n) {
+    assert.strictEqual(findOdd(a), n, `Incorrect answer for input=[${a}]`);
+  }
+
+  it("Example tests", () => {
+    doTest([7], 7);
+    doTest([0], 0);
+    doTest([1, 1, 2], 2);
+    doTest([0, 1, 0, 1, 0], 0);
+    doTest([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1], 4);
+  });
+
+  it("Fixed tests", () => {
+    doTest([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5], 5);
+    doTest([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], -1);
+    doTest([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], 5);
+    doTest([10], 10);
+    doTest([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], 10);
+    doTest([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], 1);
+  });
+});
