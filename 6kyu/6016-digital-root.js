11af191479fdd95285efc4ca73add8087269f9d5
Daniel Garcia <danvgar@gmail.com>
2023-09-18 15:30:12 -0700

    initial commit

---
+++ 6kyu/6016-digital-root.js
@@ -0,0 +1,50 @@
+// https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
+// First Attempt - Sept 18, 2023
+
+// =============
+// Problem
+// =============
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+// function digitalRoot(n) {
+//   // ...
+// }
+
+
+// ==============
+// Solution 1
+// ==============
+
+function digitalRoot(n) {
+  function accumulator(n) {
+    let sum = 0
+    n = n.toString().split("").reduce((acc, c) => {
+    +acc + +c
+    }, 0)
+  }
+}
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(digitalRoot(16), 7)
+    assert.strictEqual(digitalRoot(456), 6)
+  });
+});
