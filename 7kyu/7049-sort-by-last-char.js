b9440c6f1eba3a41440ac87e24758ea4872d916d
Daniel Garcia <danvgar@gmail.com>
2023-11-02 20:18:27 -0700

    Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each

---
+++ 7kyu/7049-sort-by-last-char.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/539ee3b6757843632d00026b/train/javascript
+// First Attempt - Nov 2, 2023
+
+
+// =============
+// Problem
+// =============
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+// function last(x){
+
+// }
+
+
+// ==============
+// Solution 1
+// ============ 
+
+function last(x){
+
+}
+
+
+// ==============
+// Tests
+// ==============
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Sort by Last Char", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+    assert.deepEqual(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
+    assert.deepEqual(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);    
+  })
+});
