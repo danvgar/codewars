984138479632e2c7fa2f7ca0b7b8159516750772
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-10-04 15:29:16 -0700

    Given an integer array, return an array of only the even integers.

--- js/7kyu/7054-form-the-minimum.js
+++ js/7kyu/7054-form-the-minimum.js
@@ -1,35 +1,78 @@
-// https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
-// First Attempt - Oct 1, 2024
+// https://www.codewars.com/kata/5ac6932b2f317b96980000ca/train/javascript
+// First Attempt - Oct 3, 2024
 
 // =============
 // Problem
 // =============
 
-// Write a small function that returns the values of an array that are not odd.
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
 
-// All values in the array will be integers. Return the good values in the order they are given.
+// Notes:
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
 
-// function noOdds( values ){
-//   // Return all non-odd values
+// Input >> Output Examples
+
+// minValue ({1, 3, 1})  ==> return (13)
+// Explanation:
+// (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+// minValue({5, 7, 5, 9, 7})  ==> return (579)
+// Explanation:
+// (579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+
+// minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+// Explanation:
+// (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+
+// function minValue(values){
+//   //your code here
 // }
 
 // ==============
 // Solution 1
 // ==============
 
-function noOdds(values) {
-  return values.filter((num) => num % 2 === 0);
+// Inputs: Integer array of positive non-zero numbers.
+// Outputs: Integer -> smallest formed using every digit at least once, but ignoring duplicates
+// Edge-cases: No edge-cases to consider, inputs will always be sanitized
+
+function minValue(values) {
+  // Smallest number is going to be the array sorted from least to greatest
+  // Create a Set to ensure no duplicates
+  const uniques = Array.from(new Set(values))
+  const lowestDigit = uniques.sort( (a,b) => a - b).join("")
+  return parseInt(lowestDigit)
 }
+
+// Tests
+console.log((minValue([1, 3, 1]), 13));
+console.log((minValue([4, 7, 5, 7]), 457));
+console.log((minValue([4, 8, 1, 4]), 148));
+console.log((minValue([5, 7, 9, 5, 7]), 579));
+console.log((minValue([6, 7, 8, 7, 6, 6]), 678));
+console.log((minValue([5, 6, 9, 9, 7, 6, 4]), 45679));
+console.log((minValue([1, 9, 1, 3, 7, 4, 6, 6, 7]), 134679));
+console.log((minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9]), 356789));
+console.log((minValue([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 1));
 
 // ==============
 // Tests
 // ==============
 
-const { assert } = require("chai");
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
 describe("Basic tests", () => {
   it("Testing for fixed tests", () => {
-    assert.deepEqual(noOdds([0, 1]), [0]);
-    assert.deepEqual(noOdds([0, 1, 2, 3]), [0, 2]);
+    assert.strictEqual(minValue([1, 3, 1]), 13);
+    assert.strictEqual(minValue([4, 7, 5, 7]), 457);
+    assert.strictEqual(minValue([4, 8, 1, 4]), 148);
+    assert.strictEqual(minValue([5, 7, 9, 5, 7]), 579);
+    assert.strictEqual(minValue([6, 7, 8, 7, 6, 6]), 678);
+    assert.strictEqual(minValue([5, 6, 9, 9, 7, 6, 4]), 45679);
+    assert.strictEqual(minValue([1, 9, 1, 3, 7, 4, 6, 6, 7]), 134679);
+    assert.strictEqual(minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9]), 356789);
+    assert.strictEqual(minValue([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 1);
   });
 });
